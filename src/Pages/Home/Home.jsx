55d77cd0fddec1e239d21f9a8fe8120323afa99e
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-10-30 14:14:35 -0700

    Navbar and google font 'Chakra Petch' added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/Banner/Banner";
+import Navbar from "../../components/Navbar/Navbar";
 
 
 const Home = () => {
     return (
-        <div className="max-w-[1280px] mx-auto">
+        <div className="max-w-[1280px] mx-auto font-font-family">
             <Banner></Banner>
+            <Navbar></Navbar>
         </div>
     );
 };
