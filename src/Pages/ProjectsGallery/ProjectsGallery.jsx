818a176a242a434a32e85cb7c843a0b41fae7241
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-12-08 13:07:23 +0600

    projects gallery added

--- src/Pages/ProjectsGallery/ProjectsGallery.jsx
+++ src/Pages/ProjectsGallery/ProjectsGallery.jsx
@@ -1,3 +1,10 @@
+import hostelImg from "../../assets/gallery1.jpg"
+import JobImg from "../../assets/jobs.webp"
+import technologyImg from "../../assets/banner (1).jpg"
+import eventImg from "../../assets/event-technology (1).jpg"
+import donationImg from "../../assets/donation.jpg"
+import flowerImg from "../../assets/photo-2.avif"
+
 
 
 const ProjectsGallery = () => {
@@ -5,42 +12,61 @@ const ProjectsGallery = () => {
         <div className=" flex flex-col justify-center items-center min-h-screen">
             <h1 className="text-5xl font-semibold mt-10 mb-8">Projects Gallery</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-1 lg:px-0 gap-5 lg:gap-8">
-                <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
-                    <div className="card-body">
-                        <h2 className="card-title">Skills</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla, repudiandae ducimus deserunt earum ab maiores quis non provident enim corporis porro asperiores, aspernatur natus iste vitae accusamus soluta fugiat molestiae harum? Quos, qui? Qui!</p>
+                <a href="https://hostel-management-9654d.web.app/" target="blank">
+                    <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
+                        <div className="p-4">
+                            <img src={hostelImg} className="rounded-xl h-[250px] object-cover w-full" alt="" />
+                            <h2 className="card-title my-2">Hostel Management</h2>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
-                    <div className="card-body">
-                        <h2 className="card-title">Skills</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla, repudiandae ducimus deserunt earum ab maiores quis non provident enim corporis porro asperiores, aspernatur natus iste vitae accusamus soluta fugiat molestiae harum? Quos, qui? Qui!</p>
+                </a>
+                <a href="https://job-finder-hub.web.app/" target="blank">
+                    <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
+                        <div className="p-4">
+                            <img src={JobImg} className="rounded-xl h-[250px] object-cover w-full" alt="" />
+                            <h2 className="card-title my-2">Job Finder Hub</h2>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
-                    <div className="card-body">
-                        <h2 className="card-title">Skills</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla, repudiandae ducimus deserunt earum ab maiores quis non provident enim corporis porro asperiores, aspernatur natus iste vitae accusamus soluta fugiat molestiae harum? Quos, qui? Qui!</p>
+                </a>
+                <a href="https://techno-and-electro-client.web.app/" target="blank">
+                    <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
+                        <div className="p-4">
+                            <img src={technologyImg} className="rounded-xl h-[250px] object-cover w-full" alt="" />
+                            <h2 className="card-title my-2">Technology & Electronics e-commerce site </h2>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
-                    <div className="card-body">
-                        <h2 className="card-title">Skills</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla, repudiandae ducimus deserunt earum ab maiores quis non provident enim corporis porro asperiores, aspernatur natus iste vitae accusamus soluta fugiat molestiae harum? Quos, qui? Qui!</p>
+                </a>
+                <a href="https://corporate-event-manageme-92f84.web.app/" target="blank">
+                    <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
+                        <div className="p-4">
+                            <img src={eventImg} className="rounded-xl h-[250px] object-cover w-full" alt="" />
+                            <h2 className="card-title my-2">Corporate Event Management</h2>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
-                    <div className="card-body">
-                        <h2 className="card-title">Skills</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla, repudiandae ducimus deserunt earum ab maiores quis non provident enim corporis porro asperiores, aspernatur natus iste vitae accusamus soluta fugiat molestiae harum? Quos, qui? Qui!</p>
+                </a>
+                <a href="https://roasted-honey.surge.sh/" target="blank">
+                    <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
+                        <div className="p-4">
+                            <img src={donationImg} className="rounded-xl h-[250px] object-cover w-full" alt="" />
+                            <h2 className="card-title my-2">Donation Campaign</h2>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
-                    <div className="card-body">
-                        <h2 className="card-title">Skills</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla, repudiandae ducimus deserunt earum ab maiores quis non provident enim corporis porro asperiores, aspernatur natus iste vitae accusamus soluta fugiat molestiae harum? Quos, qui? Qui!</p>
+                </a>
+                <a href="https://sayedul3731.github.io/my-first-assignment-website/" target="blank">
+                    <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
+                        <div className="p-4">
+                            <img src={flowerImg} className="rounded-xl h-[250px] object-cover w-full" alt="" />
+                            <h2 className="card-title my-2">Flower Shop</h2>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla.</p>
+                        </div>
                     </div>
-                </div>
+                </a>
+                
             </div>
         </div>
     );
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,11 +2,19 @@
 
 const Banner = () => {
     return (
-        <div className="flex flex-col-reverse md:flex-row justify-between items-center w-full h-[350px] mb-32 md:mb-0 md:h-[600px]">
-            <div className="w-1/2  flex justify-center md:pl-20 lg:pl-0 items-center">
-                <h1 className="text-3xl md:text-5xl font-bold text-white"><span className="">Hi</span>, I'm  <br />Front-end <span className="">Developer</span>.</h1>
+        <div className="flex flex-col-reverse md:flex-row justify-between items-center w-full mb-32 md:mb-0 md:h-[600px] ">
+            <div className="w-full  lg:-mb-[84px] md:w-1/2  flex flex-col justify-start  md:justify-center md:items-center px-3 md:px-0 md:pl-20 lg:pl-0 ">
+                <h1 className="text-3xl md:text-5xl mt-10 md:mt-0 font-bold  text-white "><span className="text-[#fc03c2]">Hi,</span> This is <br /> MD SAYEDUL HAQUE</h1>
+                <div className="">
+                    <p className="text-xl md:text-2xl font-semibold my-5 flex md:justify-center  lg:-ml-[140px] text-[#fc03c2]">I'm Front-End Web Developer</p>
+                </div>
+                <div className=" lg:pl-[244px] ">
+                    <p>
+                        From Brahmanbaria, Bangladesh. I’m a Front End Web developer who is passionate about making error-free websites. I am good at JavaScript & React. I love to talk with you about your unique.
+                    </p>
+                </div>
             </div>
-            <div className="w-1/2  flex justify-center items-center">
+            <div className="w-1/2 flex justify-center items-center">
                 <img src="https://i.ibb.co/Hzp66jb/taki-removebg-preview.png" alt="my picture" />
             </div>
         </div>
