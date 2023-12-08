878d4f3892dfdd1dc38861730729b882d75c73d3
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-12-08 14:40:49 +0600

    about me description added

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,12 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
 
 
 const AboutMe = () => {
     return (
         <div className="px-1">
-             <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
+            <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
                 <div className="card-body">
                     <h2 className="card-title font-semibold text-[#fff]">About Me</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla, repudiandae ducimus deserunt earum ab maiores quis non provident enim corporis porro asperiores, aspernatur natus iste vitae accusamus soluta fugiat molestiae harum? Quos, qui? Qui!</p>
+                    <p className="text-start">
+                        I'm Md Sayedul Haque who passionate front-end web developer with expertise in JavaScript, React, CSS, and HTML and also I know React framework Next.js and I have some knowledge on MongoDB, Node.js and Express.js. Committed to creating engaging and seamless user experiences. Proficient in translating design concepts into responsive and dynamic web applications. A problem-solver with a keen eye for detail, dedicated to staying updated on industry trends. Eager to contribute creativity and technical skills to innovative projects.
+                    </p>
                 </div>
             </div>
         </div>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,10 +3,25 @@
 const Skills = () => {
     return (
         <div className="px-1">
-             <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
+            <div className="card lg:w-[450px] bg-[#0590fa] shadow-2xl">
                 <div className="card-body">
                     <h2 className="card-title">Skills</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias officiis culpa nemo alias omnis nulla, repudiandae ducimus deserunt earum ab maiores quis non provident enim corporis porro asperiores, aspernatur natus iste vitae accusamus soluta fugiat molestiae harum? Quos, qui? Qui!</p>
+                    JavaScript
+                    <progress className="progress progress-secondary" value="60" max="100"></progress>
+                    React.js
+                    <progress className="progress progress-secondary" value="65" max="100"></progress>
+                    Next.js
+                    <progress className="progress progress-secondary" value="42" max="100"></progress>
+                    CSS
+                    <progress className="progress progress-secondary" value="55" max="100"></progress>
+                    HTML
+                    <progress className="progress progress-secondary" value="85" max="100"></progress>
+                    MongoDB
+                    <progress className="progress progress-secondary" value="40" max="100"></progress>
+                    Express.js
+                    <progress className="progress progress-secondary" value="45" max="100"></progress>
+                    Node.js
+                    <progress className="progress progress-secondary" value="42" max="100"></progress>
                 </div>
             </div>
         </div>
